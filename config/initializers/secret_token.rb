2e8ba842d060a104afda18093d005dcc8eb6dbd8
jade Thomas <jade@wmd.co.nz>
2011-07-03 19:38:50 +1200

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DesignerCurtains::Application.config.secret_token = '5c36fb60ac86bedf358d15b8ae7c24bdd6519a778aaa59ef5806c77df31717eb4acf96babd065c61fee4e58426e7fab16ff94dc08ccd265c2713b1b500112446'
